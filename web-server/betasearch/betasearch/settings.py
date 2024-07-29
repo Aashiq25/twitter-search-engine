8293fd30bf0af3245544347055a0ed0c6eb97956
Muhamed Aashiq <muhamedaashiqtce@gmail.com>
2024-07-28 22:32:17 -0500

    Remove heroku

--- web-server/betasearch/betasearch/settings.py
+++ web-server/betasearch/betasearch/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,4 +130,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORE = 'IRF21P1_demo'
 AWS_URL = '18.189.189.150'
 
-django_heroku.settings(locals())
